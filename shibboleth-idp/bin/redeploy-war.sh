1cc264ff4f3b597e61553ccb7769f070cb496970
Logan Aikas <50219221+laikas123@users.noreply.github.com>
2025-01-29 13:20:08 -0800

    Update redeploy war script.

--- shibboleth-idp/bin/redeploy-war.sh
+++ shibboleth-idp/bin/redeploy-war.sh
@@ -1,5 +1,6 @@
 rm -rf /usr/local/tomcat/webapps/idp
 rm -rf /usr/local/tomcat/webapps/idp.war
+/opt/shibboleth-idp/bin/build.sh
 cp /opt/shibboleth-idp/war/idp.war /usr/local/tomcat/webapps/idp.war
 /usr/local/tomcat/bin/shutdown.sh
 sleep 4
